2558fbc9888f1b3ac9f870862013bde704c76343
Dmitry <dimk00z@gmail.com>
2020-04-13 22:46:23 +0300

    Update duration.py
    упростил код

--- duration.py
+++ duration.py
@@ -3,13 +3,5 @@ def get_duration(duration):
     hours, remainder = divmod(td_in_seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     hours = int(hours)
-    is_strange = False
-    if hours > 1:
-        is_strange = True
-    minutes = int(minutes)
-    seconds = int(seconds)
-    if minutes < 10:
-        minutes = "{:02d}".format(minutes)
-    if seconds < 10:
-        seconds = "{:02d}".format(seconds)
-    return "{}:{}:{}".format(hours, minutes, seconds), is_strange
+    is_strange = True if hours > 1 else False
+    return "{}:{:02d}:{:02d}".format(hours, int(minutes), int(seconds)), is_strange
